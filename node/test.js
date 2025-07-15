def5c2229e50495010082b26a5e877b1b4e8b825
Ansh-ctrl-f4 <abookhaa@gmail.com>
2025-07-15 13:51:39 +0600

    Update test.js
    update #3

--- node/test.js
+++ node/test.js
@@ -1,2 +1,5 @@
-const { say } = require('../pkg/wasmedge_nodejs_starter_lib.js');
-console.log( say("Node.js") );
+const { reverse_string } = require('../pkg/wasmedge_nodejs_starter_lib.js');
+
+console.log("Testing reverse_string with 'Hello from Node.js!':", reverse_string("Hello from Node.js!"));
+console.log("Testing reverse_string with 'Rust is fast!':", reverse_string("Rust is fast!"));
+console.log("Testing reverse_string with '12345':", reverse_string("12345"));
